e6b0cc356322664e05912914424c233a81a4f084
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-06-05 20:39:35 +0900

    update mypage

--- component/myPage/EditUserImg.tsx
+++ component/myPage/EditUserImg.tsx
@@ -3,7 +3,8 @@ import { useForm } from "react-hook-form";
 import styles from "./EditUserImg.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
-import axios from "axios";
+import { Avatar, Box, Input, VStack } from "@chakra-ui/react";
+import Image from "next/image";
 
 const EditUserImg = () => {
   const { register, handleSubmit } = useForm();
@@ -11,22 +12,46 @@ const EditUserImg = () => {
   const imgSubmit = () => {};
 
   return (
-    <div className={styles.profileDiv}>
+    <VStack spacing={5}>
       <h2>프로필 정보</h2>
-      <img alt="" />
-      <form className={styles.uploadForm} onSubmit={handleSubmit(imgSubmit)}>
-        <div className={styles.uploadFile}>
-          <input type="file" {...register("file")} accept="image/*" />
+      <Avatar src="" size="2xl" />
+      <Box
+        as="form"
+        className={styles.uploadForm}
+        onSubmit={handleSubmit(imgSubmit)}
+        cursor="pointer"
+      >
+        <Box
+          position="relative"
+          top="-60px"
+          left={"-40px"}
+          borderRadius="50%"
+          width="50px"
+          height="50px"
+          lineHeight="47px"
+          backgroundColor="#fccec0"
+          textAlign="center"
+        >
+          <Input
+            type="file"
+            cursor="pointer"
+            {...register("file")}
+            accept="image/*"
+            position="absolute"
+            transform="scale(1)"
+            left={"0"}
+            top={"5px"}
+            opacity={0}
+          />
           <FontAwesomeIcon
             icon={faCamera}
             style={{ color: "white" }}
             fixedWidth
+            size="xl"
           />
-        </div>
-        <button type="submit">변경하기</button>
-        {/* {isUploadError ? <p>이미지를 다시 올려주세요.</p> : null} */}
-      </form>
-    </div>
+        </Box>
+      </Box>
+    </VStack>
   );
 };
 
