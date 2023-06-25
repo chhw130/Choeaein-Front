3b42a7e983ff9d702f43f631dd81b074c8409831
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-06-25 17:23:43 +0900

    update : font

--- UI/Card/DescriptionCard.tsx
+++ UI/Card/DescriptionCard.tsx
@@ -1,15 +1,31 @@
-import { Box, Card, CardBody, Heading, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 const DescriptionCard = () => {
   return (
-    <Card as="article" w={"60%"}>
+    <Card
+      as="article"
+      w={"50%"}
+      h={"100%"}
+      padding={5}
+      borderLeft={"8px solid black"}
+      borderRadius={0}
+    >
+      <CardHeader>
+        <Text>(소속사)</Text>
+        <Text>이름feqwfqw()</Text>
+      </CardHeader>
       <CardBody>
         <Stack spacing={2}>
-          <Box>
-            <Heading size="md">이름</Heading>
-            <Text>소속사</Text>
-          </Box>
           <Text>데뷔일</Text>
           <Text>인스타</Text>
           <Text>유튜브</Text>
--- app/ChakraUIProvider.tsx
+++ app/ChakraUIProvider.tsx
@@ -13,6 +13,26 @@ const ChakraProvider = dynamic(() =>
   import("@chakra-ui/provider").then((mod) => mod.ChakraProvider)
 );
 
+import { Roboto, Noto_Sans_KR, Homemade_Apple } from "next/font/google";
+
+const notoSansKr = Noto_Sans_KR({
+  // preload: true, 기본값
+  subsets: ["latin"], // 또는 preload: false
+  weight: ["100", "400", "700", "900"], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
+});
+
+const roboto = Roboto({
+  subsets: ["latin"], // preload에 사용할 subsets입니다.
+  weight: ["100", "400", "700"],
+  variable: "--roboto", // CSS 변수 방식으로 스타일을 지정할 경우에 사용합니다.
+});
+
+export const homemadeApple = Homemade_Apple({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+});
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const [mount, setMounted] = useState<boolean>(false);
 
@@ -27,7 +47,12 @@ export function Providers({ children }: { children: React.ReactNode }) {
     useSystemColorMode: false,
   };
 
-  const theme = extendTheme({ config });
+  const theme = extendTheme({
+    config,
+    fonts: {
+      body: homemadeApple.style.fontFamily,
+    },
+  });
 
   return (
     <ChakraProvider theme={theme}>
