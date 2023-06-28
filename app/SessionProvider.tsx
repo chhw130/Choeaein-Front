9772d177373291f549ede7d8caf8eb7f072620df
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-06-28 17:12:30 +0900

    Revert "update : sementic tag"
    This reverts commit 9394fee7efc794d1c33fd02e7f45a0c2580fba35.

--- app/SessionProvider.tsx
+++ app/SessionProvider.tsx
@@ -2,8 +2,8 @@
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-const sessionAuthProvider = ({ children }: { children: React.ReactNode }) => {
+const Provider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
 
-export default sessionAuthProvider;
+export default Provider;
