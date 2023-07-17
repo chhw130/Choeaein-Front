af202c39d23702a76716e156ab3f547f3461d8ba
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-07-17 16:20:07 +0900

    test : video

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,12 @@ import DefaultLayout from "../UI/Footer/DefaultLayout";
 import { Providers } from "./ChakraUIProvider";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "react-toastify/dist/ReactToastify.css";
-
 import { Metadata } from "next";
 import RecoilProvider from "./RecoilProvider";
 
 export const metadata: Metadata = {
   title: "Myfavor",
   description: "최애의 스케줄을 확인하세요.",
-  icons: {
-    icon: "/img/logo_main.png",
-    shortcut: "/img/logo_main.png",
-  },
 };
 
 export default function RootLayout({
