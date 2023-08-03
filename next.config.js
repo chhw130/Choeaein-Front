5edbdf992d5e2fffcb9591d27142c479bedf18a1
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-08-03 21:46:29 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,16 +6,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const API_URL = process.env.NEXT_PUBLIC_DEV_BASE_URL;
 
 const nextConfig = {
-  async rewrites() {
-    if (process.env.NODE_ENV === "development") {
-      return [
-        {
-          source: "/api/v2/:path*/",
-          destination: `${API_URL}/:path*/`,
-        },
-      ];
-    }
-  },
 
   trailingSlash: true,
 
