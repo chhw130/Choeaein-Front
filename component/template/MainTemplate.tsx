73cd99f59a35ce775a861ee5f78f13246f83ffbe
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-08-05 17:22:03 +0900

    Feat : Template Constructor

--- component/template/MainTemplate.tsx
+++ component/template/MainTemplate.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import MainVideo from "../organisms/Main/MainVideo";
+import RandomSchedule from "../organisms/Main/RandomSchedule";
+import IdolSection from "../organisms/Main/IdolSection";
+import {
+  IdolGroupType,
+  IdolSoloType,
+  RandomIdolSchedule,
+} from "@/utils/interface/interface";
 
-const MainTemplate = () => {
-  return <div></div>;
+interface MainTemplateProps {
+  idolGroupData: IdolGroupType[];
+  idolSoloData: IdolSoloType[];
+  randomSchedules: RandomIdolSchedule[];
+}
+
+const MainTemplate = ({
+  idolGroupData,
+  idolSoloData,
+  randomSchedules,
+}: MainTemplateProps) => {
+  return (
+    <>
+      <MainVideo />
+      <RandomSchedule randomSchedules={randomSchedules} />
+      <IdolSection idolGroupData={idolGroupData} idolSoloData={idolSoloData} />
+    </>
+  );
 };
 
 export default MainTemplate;
