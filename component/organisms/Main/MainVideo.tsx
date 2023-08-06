a22f54436219938a1578cfc31cc444ffef6dacb3
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-08-06 20:22:33 +0900

    Design : Header Layout

--- component/organisms/Main/MainVideo.tsx
+++ component/organisms/Main/MainVideo.tsx
@@ -5,13 +5,20 @@ const MainVideo = () => {
   return (
     /**video source */
     <>
-      <Box w={"100%"} h={"475px"} pos={"relative"} objectFit={"cover"}>
+      <Box
+        w={"100%"}
+        h={"475px"}
+        pos={"relative"}
+        objectFit={"cover"}
+        backgroundColor={"blue"}
+      >
         <iframe
           src="https://player.vimeo.com/video/740372966?autoplay=true&loop=true&muted=true&controls=false&playsinline=0&quality=1080p&transparent=0"
           allow="autoplay"
           style={{
             width: "100%",
             height: "100%",
+            backgroundColor: "blue",
           }}
         />
       </Box>
